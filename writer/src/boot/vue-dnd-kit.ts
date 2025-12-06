ad0f45c91e19401c132537e54829d6db374e8fff
Milan Košir <kosir.milan@gmail.com>
2025-12-06 11:36:58 +0100

    removed old projects

--- writer/src/boot/vue-dnd-kit.ts
+++
@@ -1,7 +0,0 @@
-import { boot } from 'quasar/wrappers';
-import VueDnDKitPlugin from '@vue-dnd-kit/core';
-
-export default boot(({ app }) => {
-  app.use(VueDnDKitPlugin);
-});
-
--- writer/src/mitt.ts
+++
@@ -1,8 +0,0 @@
-import mitt from 'mitt'
-
-// Define all event types used in the mindmap component
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-type Events = Record<string, any>
-
-const emitter = mitt<Events>()
-export default emitter
